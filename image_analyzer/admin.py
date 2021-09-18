b376a937cecdaded8f9745d2919072d31c82dce8
Yui2u <beloleg225@mail.ru>
2021-09-18 20:06:23 +0700

    Deleted superuser. Changed css.

--- image_analyzer/admin.py
+++ image_analyzer/admin.py
@@ -2,7 +2,5 @@ from django.contrib import admin
 from .models import ImageHolder # User, UserLog
 
 # Register your models here.
-admin.site.register(ImageHolder)
-# admin.site.register(User)
-# admin.site.register(UserLog)
+
 
